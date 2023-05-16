edfd910c80f37d0c787359984f78d16dab4cf014
sara-molie <125445622+sara-molie@users.noreply.github.com>
2023-05-16 17:13:40 +0200

    ajout explication

--- src/Board.java
+++ src/Board.java
@@ -1,14 +1,11 @@
-package taquinFX;
-
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-
 public class Board {
 	private Box[][] grid;
 	private int boardSize;
-	private static final String CSV_PATH = "levels/lvl1.csv";
+	private static final String CSV_PATH = "mettre le lien.csv";
 	
 	//constructeur 
 	public Board(int boardSize) {
@@ -16,40 +13,32 @@ public class Board {
 		grid = new Box [boardSize][boardSize];
 		initializeBoard();	
 	}
-	//nous allons initialiser le plateau
+	// Méthode pour initialiser le plateau de jeu
 	private void initializeBoard() {
-		 try (BufferedReader reader = new BufferedReader(new FileReader(CSV_PATH))) {
-	            String line;
-	            int row = 0;
-	            while ((line = reader.readLine()) != null) {
-	                String[] values = line.split(",");
-	                for (int col = 0; col < values.length; col++) {
-	                    int num = Integer.parseInt(values[col].trim());
-	                    grid[row][col] = new Box(num, num == 0, num == 0 ? "" : Integer.toString(num));
-	                }
-	                row++;
+	    try (BufferedReader reader = new BufferedReader(new FileReader(CSV_PATH))) { // On tente d'ouvrir le fichier CSV pour lecture
+	        String line; // Variable pour stocker chaque ligne du fichier
+	        int row = 0; // Variable pour suivre la ligne courante dans la grille
+	        while ((line = reader.readLine()) != null) {  // Boucle qui lit chaque ligne du fichier jusqu'à ce qu'il n'y en ait plus
+	            String[] values = line.split(",");// On sépare la ligne en plusieurs valeurs en utilisant la virgule comme séparateur
+	            for (int col = 0; col < values.length; col++) { // Boucle sur chaque valeur de la ligne
+	                int num = Integer.parseInt(values[col].trim()); // On convertit la valeur en nombre entier
+	                grid[row][col] = new Box(num, num == 0, num == 0 ? "" : Integer.toString(num));  // On crée une nouvelle case (Box) avec cette valeur et on l'ajoute à la grille
 	            }
-	        } catch (IOException e) {
-	            e.printStackTrace();
+	            row++;  // On incrémente le compteur de ligne pour passer à la ligne suivante
 	        }
+	    // Si une exception d'entrée/sortie se produit (par exemple si le fichier n'est pas trouvé), on l'affiche
+	    } catch (IOException e) {
+	        e.printStackTrace();
 	    }
-	
-//Méthode pour afficher le plateau
-public void displayBoard() {
-    for (int i = 0; i < boardSize; i++) {
-        for (int j = 0; j < boardSize; j++) {
-            System.out.print(grid[i][j].getDisplay() + " ");
-        }
-        System.out.println();
-    }
-}
+	}   
 
-public static void main(String[] args) {
-    // Créer un nouveau plateau de taille 3
-    Board board = new Board(3);
-
-    // Afficher le plateau
-    board.displayBoard();
-}
-
+	// Méthode pour afficher le plateau de jeu
+	public void displayBoard() {
+	    for (int i = 0; i < boardSize; i++) {  // On boucle sur chaque ligne de la grille
+	        for (int j = 0; j < boardSize; j++) { // On boucle sur chaque colonne de la ligne
+	            System.out.print(grid[i][j].getDisplay() + " ");  // On affiche la représentation de la case
+	        }
+	        System.out.println(); // On passe à la ligne suivante dans la console
+	    }
+	}
 }
