7344de485d8bc5c10b852a7263afa7451a911be5
sara-molie <125445622+sara-molie@users.noreply.github.com>
2023-05-16 16:51:52 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Créer un nouveau plateau de taille 3
+        Board board = new Board(3);
+
+        // Afficher le plateau
+        board.displayBoard();
+    }
+}
