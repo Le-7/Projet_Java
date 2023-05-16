8299dcb5aeda6b03bd263003c808f573c05a1dd2
sara-molie <125445622+sara-molie@users.noreply.github.com>
2023-05-16 16:41:53 +0200

    Add files via upload

---
+++ src/Board.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+public class Board {
+	private Box[][] grid;
+	private int boardSize;
+	private static final String CSV_PATH = "mettre le lien.csv";
+	
+	//constructeur 
+	public Board(int boardSize) {
+		this.boardSize = boardSize;
+		grid = new Box [boardSize][boardSize];
+		initializeBoard();	
+	}
+	//nous allons initialiser le plateau
+	private void initializeBoard() {
+		 try (BufferedReader reader = new BufferedReader(new FileReader(CSV_PATH))) {
+	            String line;
+	            int row = 0;
+	            while ((line = reader.readLine()) != null) {
+	                String[] values = line.split(",");
+	                for (int col = 0; col < values.length; col++) {
+	                    int num = Integer.parseInt(values[col].trim());
+	                    grid[row][col] = new Box(num, num == 0, num == 0 ? "" : Integer.toString(num));
+	                }
+	                row++;
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	
+//Méthode pour afficher le plateau
+public void displayBoard() {
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
+            System.out.print(grid[i][j].getDisplay() + " ");
+        }
+        System.out.println();
+    }
+}
+
+}
