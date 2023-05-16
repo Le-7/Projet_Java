86fc2762a231c93762bfad68a15b7ed11c888743
sara-molie <125445622+sara-molie@users.noreply.github.com>
2023-05-16 15:48:09 +0200

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,25 @@
+public class Box{
+	
+	private int value;
+	private boolean movable;
+	private String display;
+	
+	public Box(int value, boolean movable, String display) {
+		this.value = value;
+		this.movable = movable;
+		this.display=display;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public boolean getMovable() {
+		return movable;
+	}
+	
+	public String getDisplay() {
+		return display;
+		
+	}
+}
